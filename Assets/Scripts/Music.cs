a3baf22f633fb12abc3c063a3311151af94fea1c
Reza Mirzaee <rmirzaee95@gmail.com>
2017-10-20 16:47:38 +0330

    Home Collapse added

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Music : MonoBehaviour {
     private static Music instance = null;
     private AudioSource source;
@@ -18,9 +18,18 @@ public class Music : MonoBehaviour {
             instance = this;
             source = GetComponent<AudioSource>();
             Load_Musics();
-            int i = 2;
-            Music.instance.source.PlayOneShot(Music.instance.sounds[i]);
-            StartCoroutine(MusicShuffle(Music.instance.sounds[i].length));
+            int i = Random.Range(1, 3);
+            if (SceneManager.GetActiveScene().name == "Start")
+            {
+                Music.instance.source.PlayOneShot(Music.instance.sounds[0]);
+                StartCoroutine(MusicShuffle(Music.instance.sounds[0].length));
+            }
+            else
+            {
+                Music.instance.source.PlayOneShot(Music.instance.sounds[i]);
+                StartCoroutine(MusicShuffle(Music.instance.sounds[i].length));
+            }
+          
         }
 
         DontDestroyOnLoad(this.gameObject);
@@ -28,7 +37,7 @@ public class Music : MonoBehaviour {
     IEnumerator MusicShuffle(float time)
     {
         yield return new WaitForSeconds(time);
-        int i = Random.Range(0, 3);
+        int i = Random.Range(1, 3);
         Music.instance.source.Stop();
         Music.instance.source.PlayOneShot(Music.instance.sounds[i]);
         StartCoroutine(MusicShuffle(Music.instance.sounds[i].length));
