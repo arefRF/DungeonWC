ec0f8fba601b1e70eaa76deeffbbbee118c92113
Reza Mirzaee <rmirzaee95@gmail.com>
2017-10-20 17:04:37 +0330

    Collapse Animation

---
+++ Assets/Scripts/CollapseHome.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollapseHome : MonoBehaviour {
+    public GameObject home;
+    void OnTriggerEnter2D(Collider2D col){
+        if (col.name == "Player")
+        {
+            home.GetComponent<Animator>().SetTrigger("Collapse");
+        }
+    }
+}
