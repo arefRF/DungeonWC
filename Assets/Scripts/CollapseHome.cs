b50343f79c9fb47d2dbeba88b30f2f402fabe26e
Reza Mirzaee <rmirzaee95@gmail.com>
2017-10-20 17:16:50 +0330

    Collapse Animation Completed

--- Assets/Scripts/CollapseHome.cs
+++ Assets/Scripts/CollapseHome.cs
@@ -7,7 +7,9 @@ public class CollapseHome : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D col){
         if (col.name == "Player")
         {
+            col.GetComponentInChildren<Animator>().SetTrigger("Fall");
             home.GetComponent<Animator>().SetTrigger("Collapse");
+            
         }
     }
 }
