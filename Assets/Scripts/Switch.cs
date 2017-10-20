679c9f736cb06380c692f708392d5526aa1ddff8
Reza Mirzaee <rmirzaee95@gmail.com>
2017-10-20 18:15:04 +0330

    Switch Sound

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -3,17 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Switch : Unit{
-
+    private AudioSource source;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
     public End endtile { get; set; }
     public bool isOn = false;
 
-    // Use this for initialization
-    void Awake () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void UnlockSound(){
+        source.Play();
+    }
 }
