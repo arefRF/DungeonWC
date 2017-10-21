1c50e565d9f6d9a994f46a1cbc865a9b20e25bde
aref <valipour.aref@gmail.com>
2017-10-21 13:13:31 +0330

    ad

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -81,7 +81,10 @@ public class Player : Unit {
         {
             engine.AddToSnapshot(Clone());
             for (int i = 0; i < engine.enemies.Count; i++)
-                engine.AddToSnapshot(engine.enemies[i].Clone());
+            {
+                if(engine.enemies[i] is Enemy_Mage && (engine.enemies[i] as Enemy_Mage).FireballCharged)
+                    engine.AddToSnapshot(engine.enemies[i].Clone());
+            }
             prevpos = Position;
             Position = temppos;
             a_event.dir = direction;
